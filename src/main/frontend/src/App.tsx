c96b3b1c3e22bf30601fee2de6af6a3041f15b42
jisooround <9utesleben@gmail.com>
2024-04-02 23:58:09 +0900

    F_ENV: 타입스크립트 설정

---
+++ src/main/frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
